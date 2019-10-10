8c52a0c1feee614c667624101c72228e5b226b37
kinghuin <kinghuin_chull@163.com>
2019-10-10 14:23:11 +0800

    debug

--- PaddleNLP/models/sequence_labeling/nets.py
+++ PaddleNLP/models/sequence_labeling/nets.py
@@ -48,7 +48,7 @@ def lex_net(word, length, args, vocab_size, num_labels, for_infer = True, target
                 regularizer=fluid.regularizer.L2DecayRegularizer(
                     regularization_coeff=1e-4)))
 
-        gru = fluid.layers.rnn.rnn(cell=gru_cell, inputs=pre_gru, sequence_length=length)
+        gru,_ = fluid.layers.rnn.rnn(cell=gru_cell, inputs=pre_gru, sequence_length=length)
 
         pre_gru_r = fluid.layers.fc(
             input=input_feature,
@@ -60,7 +60,7 @@ def lex_net(word, length, args, vocab_size, num_labels, for_infer = True, target
                     regularization_coeff=1e-4)),
             num_flatten_dims=2)
 
-        gru_r = fluid.layers.rnn.rnn(cell=gru_cell, inputs=pre_gru_r, sequence_length=length,is_reverse=True)
+        gru_r,_ = fluid.layers.rnn.rnn(cell=gru_cell, inputs=pre_gru_r, sequence_length=length,is_reverse=True)
 
         bi_merge = fluid.layers.concat(input=[gru, gru_r], axis=2)
         return bi_merge
