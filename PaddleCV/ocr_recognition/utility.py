f6e3bf41f6fcca636ae53b566dfc881970d97ed1
kinghuin <kinghuin_chull@163.com>
2019-10-12 17:59:39 +0800

    debug reader

--- PaddleCV/ocr_recognition/utility.py
+++ PaddleCV/ocr_recognition/utility.py
@@ -64,17 +64,17 @@ def add_arguments(argname, type, default, help, argparser, **kwargs):
 
 
 def to_lodtensor(data, place):
-    seq_lens = [len(seq) for seq in data]
+    # seq_lens = [len(seq) for seq in data]
     cur_len = 0
-    lod = [cur_len]
-    for l in seq_lens:
-        cur_len += l
-        lod.append(cur_len)
+    # lod = [cur_len]
+    # for l in seq_lens:
+        # cur_len += l
+        # lod.append(cur_len)
     flattened_data = np.concatenate(data, axis=0).astype("int32")
-    flattened_data = flattened_data.reshape([len(flattened_data), 1])
+    # flattened_data = flattened_data.reshape([len(flattened_data), 1])
     res = fluid.LoDTensor()
     res.set(flattened_data, place)
-    res.set_lod([lod])
+    # res.set_lod([lod])
     return res
 
 
@@ -85,13 +85,15 @@ def get_ctc_feeder_data(data, place, need_label=True):
         list(map(lambda x: x[0][np.newaxis, :], data)),
         axis=0).astype("float32")
     pixel_tensor.set(pixel_data, place)
-    # label_tensor = to_lodtensor(list(map(lambda x: x[1], data)), place)
-    label_tensor = fluid.LoDTensor()
-    label_tensor.set(list(map(lambda x: x[1], data)), place)
-    img_length_tensor = fluid.LoDTensor()
-    img_length_tensor.set(list(map(lambda x: x[2], data)), place)
-    length_tensor=fluid.LoDTensor()
-    img_length_tensor.set(list(map(lambda x: x[3], data)), place)
+    label_tensor = to_lodtensor(list(map(lambda x: x[1], data)), place)
+    img_length_tensor = to_lodtensor(list(map(lambda x: x[2], data)), place)
+    length_tensor = to_lodtensor(list(map(lambda x: x[3], data)), place)
+    # label_tensor = fluid.LoDTensor()
+    # label_tensor.set(list(map(lambda x: x[1], data)), place)
+    # img_length_tensor = fluid.LoDTensor()
+    # img_length_tensor.set(list(map(lambda x: x[2], data)), place)
+    # length_tensor=fluid.LoDTensor()
+    # img_length_tensor.set(list(map(lambda x: x[3], data)), place)
 
     # img_length_tensor = to_lodtensor(list(map(lambda x: x[2], data)), place)
     # length_tensor = to_lodtensor(list(map(lambda x: x[3], data)), place)
