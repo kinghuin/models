475992330782d22daec05f4f32d177a63e8b03b7
kinghuin <kinghuin_chull@163.com>
2019-10-12 17:37:33 +0800

    debug reader

--- PaddleCV/ocr_recognition/utility.py
+++ PaddleCV/ocr_recognition/utility.py
@@ -85,7 +85,7 @@ def get_ctc_feeder_data(data, place, need_label=True):
         list(map(lambda x: x[0][np.newaxis, :], data)),
         axis=0).astype("float32")
     pixel_tensor.set(pixel_data, place)
-    label_tensor = to_lodtensor((data[1]), place)
+    label_tensor = to_lodtensor(list(map(lambda x: x[1], data)), place)
     img_length_tensor = fluid.LoDTensor()
     img_length_tensor.set(data[2], place)
     length_tensor=fluid.LoDTensor()
